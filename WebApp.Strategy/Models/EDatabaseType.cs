0218ce527add0cdefd90212b00179a184961a215
ismailtopcu <ismailtopcu.92@gmail.com>
2023-09-07 13:08:28 +0300

    Strategy Design Pattern Finish

---
+++ WebApp.Strategy/Models/EDatabaseType.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore.SqlServer.Design.Internal;
+
+namespace WebApp.Strategy.Models
+{
+    public enum EDatabaseType
+    {
+        SqlServer=1,
+        MongoDb=2
+
+    }
+}
