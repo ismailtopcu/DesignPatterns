4a1ec10578c065ec3573eed01b835a249cee96d4
ismailtopcu <ismailtopcu.92@gmail.com>
2023-09-14 13:31:42 +0300

    Chain Of Responsibility Design Pattern

---
+++ WebApp.ChainOfResponsibility/ChainOfResponsibility/ProcessHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace WebApp.ChainOfResponsibility.ChainOfResponsibility
+{
+    public abstract class ProcessHandler : IProcessHandler
+    {
+        private IProcessHandler nextProcessHandler;
+        public virtual object Handle(object o)
+        {
+            if(nextProcessHandler == null)
+            {
+                return nextProcessHandler.Handle(o);
+            }
+            return null;
+        }
+
+        public IProcessHandler SetNext(IProcessHandler processHandler)
+        {
+            nextProcessHandler = processHandler;
+            return nextProcessHandler;
+        }
+    }
+}
---
+++ WebApp.ChainOfResponsibility/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using BaseProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.ChainOfResponsibility.ChainOfResponsibility;
+using WebApp.ChainOfResponsibility.Models;
+
+namespace BaseProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly AppIdentityDbContext _context;
+
+        public HomeController(ILogger<HomeController> logger, AppIdentityDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        public async Task<IActionResult> SendEmail()
+        {
+            var products = await _context.Products.ToListAsync();
+            var excelProcessHandler = new ExcelProcessHandler<Product>();
+            var zipFileProcessHandler = new ZipFileProcessHandler<Product>();
+            var sendEmailProcessHandler = new SendEmailProcessHandler("product.zip","ismailtopcu92@gmail.com");
+
+            excelProcessHandler.SetNext(zipFileProcessHandler).SetNext(sendEmailProcessHandler);
+            excelProcessHandler.Handle(products);
+            return View(nameof(Index));
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
